7ce90ab938fcb859cec5b57195ac00069efaa148
twizelissa <twizelissa@gmail.com>
2025-06-10 14:13:31 +0200

    changed setup

--- alumathgroup13/setup.py
+++ alumathgroup13/setup.py
@@ -5,35 +5,33 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="group13-matrix-operations-2025",
-    version="1.0.0",
+    version="1.0.1",
     author="Group13",
-    author_email="e.twizeyima@alustudent.com",
+    author_email="group13@example.com",
     description="Advanced Linear Algebra Matrix Operations for Python",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/twizelissa/alugroup13-library.git",
+    url="https://github.com/group13/group13-matrix-operations-2025",
     packages=find_packages(),
     classifiers=[
         "Development Status :: 4 - Beta",
         "Intended Audience :: Developers",
         "Intended Audience :: Science/Research",
         "Topic :: Scientific/Engineering :: Mathematics",
-        "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
+        "Operating System :: OS Independent",
     ],
     python_requires=">=3.7",
     install_requires=[],
-    extras_require={
-        "dev": ["pytest>=6.0", "black", "flake8"],
-    },
-    keywords="linear algebra, matrix, multiplication, mathematics, numpy",
+    keywords="linear algebra, matrix, multiplication, mathematics",
     project_urls={
-        "Bug Reports": "https://github.com/twizelissa/alugroup13-library.gitissues",
-        "Source": "https://github.com/twizelissa/alugroup13-library.gitalumathgroup13",
+        "Bug Reports": "https://github.com/group13/group13-matrix-operations-2025/issues",
+        "Source": "https://github.com/group13/group13-matrix-operations-2025",
     },
+    license_files=["LICENSE"],  # Updated format
 )
