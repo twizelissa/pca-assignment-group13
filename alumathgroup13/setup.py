323b06f225afb1faab67469fc36dbaea28253bcc
Umwanankabandi-liliane <l.umwananka1@alustudent.com>
2025-06-10 14:20:54 +0200

    added changes

--- alumathgroup13/setup.py
+++ alumathgroup13/setup.py
@@ -16,22 +16,13 @@ setup(
     classifiers=[
         "Development Status :: 4 - Beta",
         "Intended Audience :: Developers",
-        "Intended Audience :: Science/Research",
-        "Topic :: Scientific/Engineering :: Mathematics",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
-        "Operating System :: OS Independent",
     ],
     python_requires=">=3.7",
     install_requires=[],
-    keywords="linear algebra, matrix, multiplication, mathematics",
-    project_urls={
-        "Bug Reports": "https://github.com/group13/group13-matrix-operations-2025/issues",
-        "Source": "https://github.com/group13/group13-matrix-operations-2025",
-    },
-    license_files=["LICENSE"],  # Updated format
 )
